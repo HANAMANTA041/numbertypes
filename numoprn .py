18f39015c320b9cb24cdfdb3aef596bb89401fc5
HANAMANTA <111106091+HANAMANTA041@users.noreply.github.com>
2022-08-13 07:21:57 +0530

    numoprn.md
     operations of numbers

---
+++ numoprn .py
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+
+x = (a+b)
+y = (a-b)
+z = (a//b)  # floor division
+p = (a*b)
+q = (a**b)  # exponent
+print(a/b)
+
+
+print(a)
+print(b)
+print(x)
+print(y)
+print(z)
+print(p)
+print(q)
