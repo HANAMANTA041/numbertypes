23afdf2264e699509c9efd8e61534331d308db0f
HANAMANTA <111106091+HANAMANTA041@users.noreply.github.com>
2022-08-13 06:56:08 +0530

    typeconv.md
    number type conversion in python

---
+++ typeconv.py
@@ -0,0 +1,22 @@
+# int
+# float
+# complex
+
+x = 1  # int
+y = 1.0  # float
+z = 1j  # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# convert from int to float
+a = float(x)
+# float to int
+b = int(y)
+# int to complex
+c = complex(x)
+print(a)
+print(b)
+print(c)
+
